62b462c8ec53614ce4f8f10f1d82f8eb499cd470
Cintia-Alvarado <lissbethobi25@gmail.com>
2023-07-31 21:18:54 -0800

    U20231421(Declaración de Variables)

---
+++ U20231421_Semana2.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            //DECLARACIÓN DE VARIABLE.
+
+            //ITEMS 1 (4 variables de tipo string)
+            string Primer_Nombre = "Cintia";
+            string Segundo_Nombre = "Lissbeth";
+            string Primer_Apellido = "Alvarado";
+            string Segundo_Apellido = "Robles";
+            
+
+            //ITEM 2 (3 variables de tipo int)
+            int Mi_Edad = "19";
+            int Mamá_Edad = "43";
+            int Papá_Eda =  "48";     
+
+            //ITEMS 3 (3 variables de tipo float) 
+            float  Pago_Luz = 45.85F;
+            float  Cuota_Mensual = 110.00F;
+            float  Tramporte_Diario = 3:00F;
+           
+            //ITEMS 4 (2 variables de tipo boolean)
+            bool Activo_Clases = true;
+            bool Es_Becado = false;
+           
+            //Autoevaluación: 9.0
+        }
+        
+
+
+
+
+    }
+}
